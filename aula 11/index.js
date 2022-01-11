8241828866c16acd89c24b2967e94d4a8d7db8c5
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-01-11 14:05:55 -0300

    Nova aula
    adcionando a aula 11

---
+++ aula 11/index.js
@@ -0,0 +1,14 @@
+//Sobre math e números em javascript
+
+let num1 = 1;//número
+let num2 = 2.5;//número
+
+console.log(num1 + num2);
+
+//No javascript também é possível concatenar números para isso criamos um parametro para a variavel com o .toString()
+
+//Exemplo:
+
+console.log(num1.toString + num2);
+
+//
--- aula 2/index.js
+++ aula 2/index.js
@@ -1,8 +1,11 @@
 // Escrever um comentário
+
 console.log('Olá mundo') // Outro comentário
 console.log('linha 1');
 console.log('linha 2');
 console.log('Linha 3');
 console.log('Linha 4');
+
 //Estou sendo ignorado pelo motor do JS
+
 console.log()
