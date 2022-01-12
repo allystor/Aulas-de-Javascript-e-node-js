354faa903568e781780283b3ab913f1a4e51dcbb
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-01-12 19:47:06 -0300

    Update commit
    Adicionando a aula 14

---
+++ aula 14/index.js
@@ -0,0 +1,14 @@
+//Sobre arrays
+
+//Em javascript arrays podem ser descrito como listas ou uma seleção de coleção de strings e números podendo ser manipulados
+//Dentro de um array de maneira simples
+
+//Strings são indexadas por seus caracteres em uma contagem a partir de 0, ou seja, 'Luiz'
+//                                                                                   0123
+//Já os arrays são indexados pelos seus elementos também por uma contagem de 0, como por exemplo: ['José', 'Maria', 'Ana']
+//                                                                                                    0       1       2
+//trabalhando com strings dentro de 1 array:
+
+const lista1 =['Miguel','João','Eduarda']
+
+console.log(lista1[0])
