e3817ef99463340c49167663ebb5393edb3d70cd
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-01-12 20:32:09 -0300

    Update commit

--- aula 13/main.js
+++ aula 13/main.js
@@ -7,11 +7,11 @@
 
 const numero = Number(prompt("Bem-vindo digite um número"));
 
-document.getElementById('resultado').innerHTML = numero
+document.getElementById('resultado').innerHTML = numero;
 
-document.body.innerHTML +=`A raiz quadrada de seu número é inteiro: <strong>${Math.sqrt(numero)}</strong>`
-document.body.innerHTML +=`${numero} este número é: <strong>${Number.isInteger(numero)}</strong>`
-document.body.innerHTML +=`Este número é NaN: <strong>${Number.isNaN(numero)}</strong>`
-document.body.innerHTML +=`Este número arendondado para para o seu inferior é igual à: <strong>${Math.floor(numero.toFixed(2))}</strong>`
-document.body.innerHTML +=`Este número arendondado para para o seu superior é igual à: <strong>${Math.ceil(numero.toFixed(2))}</strong>`
-document.body.innerHTML +=`Este número com casas decimais é igual à: <strong>${(numero.toFixed(2))}</strong>`
+document.body.innerHTML +=`A raiz quadrada de seu número é inteiro: <strong>${Math.sqrt(numero)}</strong>`;
+document.body.innerHTML +=`${numero} este número é: <strong>${Number.isInteger(numero)}</strong>`;
+document.body.innerHTML +=`Este número é NaN: <strong>${Number.isNaN(numero)}</strong>`;
+document.body.innerHTML +=`Este número arendondado para para o seu inferior é igual à: <strong>${Math.floor(numero.toFixed(2))}</strong>`;
+document.body.innerHTML +=`Este número arendondado para para o seu superior é igual à: <strong>${Math.ceil(numero.toFixed(2))}</strong>`;
+document.body.innerHTML +=`Este número com casas decimais é igual à: <strong>${(numero.toFixed(2))}</strong>`;
--- aula 14/index.js
+++ aula 14/index.js
@@ -1,28 +1,36 @@
 //Sobre arrays
 
 //Em javascript arrays podem ser descrito como listas ou uma seleção de coleção de strings e números podendo ser manipulados
-//Dentro de um array de maneira simples
+//dentro de um array de maneira simples.
 
-//Strings são indexadas por seus caracteres em uma contagem a partir de 0, ou seja, 'Luiz'
+//strings são indexadas por seus caracteres em uma contagem a partir de 0, ou seja, 'Luiz'.
 //                                                                                   0123
-//Já os arrays são indexados pelos seus elementos também por uma contagem de 0, como por exemplo: ['José', 'Maria', 'Ana']
+//já os arrays são indexados pelos seus elementos também por uma contagem de 0, como por exemplo: ['José', 'Maria', 'Ana'].
 //                                                                                                    0       1       2
 //trabalhando com strings dentro de 1 array:
 
-const lista1 =['Miguel','João','Eduarda']
+const lista1 =['Miguel','João','Eduarda'];
 
-console.log(lista1)
+console.log(lista1);
 
 //Se caso quisermos descobrir o tamanho do nosso array utilizamos o parâmetro .lenght
 
-console.log(lista1.length)
+console.log(lista1.length);
 
 //Se quisermos adicionarmos um novo elemento por fora do array nós automaticamente estamos adicionando um novo indice para ele
-//como neste exemplo:
+//Como neste exemplo:
 
-lista1[lista1.length] = 'miguel'
-lista1[lista1.length] = 'João'
-lista1[lista1.length] = 'Eduarda'
+lista1[lista1.length] = 'miguel';
+lista1[lista1.length] = 'João';
+lista1[lista1.length] = 'Eduarda';
 
 console.log(lista1);
+console.log(lista1.length);
+
+//Podemos realizar a mesma operação de maneira mais simples também com o parâmetro .push porém adcionando no fim
+//Exemplo:
+
+lista1.push('Olavo')
+
+console.log(lista1)
 console.log(lista1.length)
