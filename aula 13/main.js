bc88e64d7a7644723e20e832a204b395b7d27cf7
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-01-12 19:02:56 -0300

    Update commit
    atualização da aula 13

--- aula 13/main.js
+++ aula 13/main.js
@@ -5,12 +5,12 @@
 //Arredondar para um número mais inferior e superior próximo e
 //Mostra-lo com casas decimais. 
 
-const numero = prompt("Bem-vindo digite um número")
-const resultado = numero
-document.getElementById('resultado').innerHTML = resultado
+const numero = Number(prompt("Bem-vindo digite um número"));
+
+document.getElementById('resultado').innerHTML = numero
 document.body.innerHTML +=`A raiz quadrada de seu número é inteiro: <strong>${Math.sqrt(numero)}</strong>`
 document.body.innerHTML +=`${numero} este número é: <strong>${Number.isInteger(numero)}</strong>`
 document.body.innerHTML +=`Este número é NaN: <strong>${Number.isNaN(numero)}</strong>`
-document.body.innerHTML +=`Este número arendondado para para o seu inferior é igual à: <strong>${Math.ceil(numero)}</strong>`
-document.body.innerHTML +=`Este número arendondado para para o seu superior é igual à: <strong>${Math.ceil(numero)}</strong>`
-document.body.innerHTML +=`Este número com casas decimais é igual à: <strong>${Number.toFixed(numero)}</strong>`
+document.body.innerHTML +=`Este número arendondado para para o seu inferior é igual à: <strong>${Math.floor(numero.toFixed(2))}</strong>`
+document.body.innerHTML +=`Este número arendondado para para o seu superior é igual à: <strong>${Math.ceil(numero.toFixed(2))}</strong>`
+document.body.innerHTML +=`Este número com casas decimais é igual à: <strong>${(numero.toFixed(2))}</strong>`
