f258799f9efb17f2b8def690c3867dbb12ab169c
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-01-13 14:11:33 -0300

    Update commit

--- aula 13/main.js
+++ aula 13/main.js
@@ -9,9 +9,9 @@ const numero = Number(prompt("Bem-vindo digite um número"));
 
 document.getElementById('resultado').innerHTML = numero;
 
-document.body.innerHTML +=`A raiz quadrada de seu número é inteiro: <strong>${Math.sqrt(numero)}</strong>`;
-document.body.innerHTML +=`${numero} este número é: <strong>${Number.isInteger(numero)}</strong>`;
-document.body.innerHTML +=`Este número é NaN: <strong>${Number.isNaN(numero)}</strong>`;
-document.body.innerHTML +=`Este número arendondado para para o seu inferior é igual à: <strong>${Math.floor(numero.toFixed(2))}</strong>`;
-document.body.innerHTML +=`Este número arendondado para para o seu superior é igual à: <strong>${Math.ceil(numero.toFixed(2))}</strong>`;
+document.body.innerHTML +=`A raiz quadrada de seu número é inteiro: <strong>${Math.sqrt(numero)}</strong><br>`;
+document.body.innerHTML +=`${numero} este número é inteiro: <strong>${Number.isInteger(numero)}</strong><br>`;
+document.body.innerHTML +=`Este número é NaN: <strong>${Number.isNaN(numero)}</strong><br>`;
+document.body.innerHTML +=`Este número arendondado para para o seu inferior é igual à: <strong>${Math.floor(numero)}</strong><br>`;
+document.body.innerHTML +=`Este número arendondado para para o seu superior é igual à: <strong>${Math.ceil(numero)}</strong><br>`;
 document.body.innerHTML +=`Este número com casas decimais é igual à: <strong>${(numero.toFixed(2))}</strong>`;
