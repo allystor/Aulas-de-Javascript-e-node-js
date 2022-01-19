9ec8923769b04dadf82a2805ab80e56ffa34e9dd
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-01-19 20:07:39 -0300

    Update commit
    Novo conteúdo da aula 16

--- aula 16/index.js
+++ aula 16/index.js
@@ -6,6 +6,8 @@ ter propriedades, que definem suas características.
 exemplo de objeto:
 
 */
+/* Criando um objeto
+
 const pessoa1 = {
     nome:'Álvaro',
     sobrenome:'Veiga',
@@ -19,9 +21,28 @@ const pessoa2 = {
 console.log(pessoa1.nome)
 console.log(pessoa2.nome)
 
-/*Como pode se observar, os objetos possuem caracterísitcas dentro deles que podem ser acessadas pelo seu nome
+*/
+/*
+
+Como pode se observar, os objetos possuem caracterísitcas dentro deles que podem ser acessadas pelo seu nome
 e a característica que está dentro dele.
 
-para tornar este exemplo menos trabalhoso e repetitivo podemos utilizar funções para isso
+para tornar este exemplo menos trabalhoso e repetitivo podemos utilizar funções para isso retornando o objeto.
 
+Esta função também pode ser conhecida como fábrica pois ela cria novos objetos.
 */
+
+function criaPessoa (nome, sobrenome, idade) {
+    return{
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade
+    };
+}
+const pessoa1 = criaPessoa('Luiz','Ricardo',26)
+const pessoa2 = criaPessoa('Álvaro','Veiga',25)
+const pessoa3 = criaPessoa('Maria','Joaquina',24)
+
+console.log(pessoa1.nome)
+console.log(pessoa2.nome)
+console.log(pessoa3.nome)
