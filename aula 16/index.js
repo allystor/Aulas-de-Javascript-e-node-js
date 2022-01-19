99b4efa3dc66f947f302b89a8a04785877474a28
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-01-19 19:27:53 -0300

    Update commit
    Adicionando aula 16

---
+++ aula 16/index.js
@@ -0,0 +1,8 @@
+/*Sobre objetos:
+No javascript,um objeto é uma entidade independente, com propriedades e tipos. Compare-o com uma xícara,
+por exemplo. Uma xícara é um objeto, com propriedades. Da mesma forma, objetos em JavaScript podem 
+ter propriedades, que definem suas características.
+
+exemplo de objeto:
+
+*/
