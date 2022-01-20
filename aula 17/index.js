6fcb7c85d414bf4d95a9dafea808201af6a17430
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-01-20 13:56:53 -0300

    Update commit
    Adicionando aula 17

---
+++ aula 17/index.js
@@ -0,0 +1,10 @@
+/*Sobre valores primitivos e valores por referência*/
+/*
+Em JavaScript têm valores primitivos: Undefined, Null, Boolean, String e Number. Valores primitivos como 
+undefined , null , true , "texto" e 10 são imutáveis. Undefined e Null são tipos especiais que possuem um só 
+valor cada, respectivamente undefined e null .
+
+Já os valores por referênciam valor de referência é dinâmico, o JavaScript armazena o valor de referência no heap.
+Se o valor for um tipo primitivo, ao acessar a variável, você manipula o valor real armazenado nessa variável. 
+Em outras palavras, a variável que armazena um valor primitivo é acessada por por valor.
+*/
