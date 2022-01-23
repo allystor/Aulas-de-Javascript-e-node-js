7f639539c7f330141fea2504ecae6d4ae2181f15
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-01-23 09:19:22 -0300

    Update commit
    conteúdo novo para aula 17

--- aula 17/index.js
+++ aula 17/index.js
@@ -20,7 +20,31 @@ console.log(a,b)
 
 Quando criamos um dado de um valor primitívo imutável, significa que o dado se torna único dentro de suas pro-
 priedades, ou seja, se criamos a variável 'a' e concatenamos ela a um dado primitivo como uma string e  criamos
-outra string que no caso possui a unica função de cópia da variável 'a', ela receberá apenas uma vez o valor de
-'a', logo se alterarmos o dado para outro valor a outra string perderá a função de cópiar a variável 'a'
+outra variável 'b' com o valor de string que no caso possui a unica função de cópia da variável 'a', 
+ela receberá apenas uma vez o valor de 'a', logo se alterarmos o dado para outro valor a outra string
+perderá a função de cópiar a variável 'a', ou seja, 'a' se torna um valor imutável.
+
+*/
+
+/*
+
+Os próprios valores por referencia são valores mutaveis, ou seja, mutável é o tipo da variável que pode ser 
+alterada. Em JavaScript, somente objetos e arrays são mutáveis, valores primitivos não. Um objeto mutável é um 
+objeto cujo estado pode ser modificado após sua criação. Imutáveis são os objetos cujo o estado não podem ser 
+mudados uma vez criados. Como exemplos de valores mutáveis os mais conhecidos são os arrays, objetos e funções.
+
+*/
+let c = [1, 2, 3];
+let d = c;
+console.log(c, d);
+a.push(4);
+console.log(c, d);
+
+/*
+
+Quando criamos um array 'c' e concaternamos outro array 'd' ambas alteções que fizermos dentro de qualquer
+um deles, ambos terão o resultado dessa alteração como no exemplo acima onde criamos uma sequencia de
+números para os dois arrays e inserimos um número a mais para 'c' o array 'd' também adiciona o valor para
+seu array.
 
 */
