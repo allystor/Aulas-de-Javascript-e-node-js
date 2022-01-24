8030f1dc658c381fe495d80f3c7d00f61c719043
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-01-23 22:06:42 -0300

    Update commit
    Adicionado exercício na aula 8 a qual passara por correção

---
+++ aula 18/js/script.js
@@ -0,0 +1,35 @@
+/*
+Exercício sobre arrays, objetos e funções.
+
+desenvolvea um programa que peça ao usuário que insira nome, sobrenome,
+altura e peso para armazenar todos os dados dentro de um array a cada
+credencial enviada.
+
+*/
+
+function escopo() {
+    const form = document.querySelector('.form');
+    const resultado = document.getElementById('resultado');
+    const pessoas = [];
+
+    function recebeEventoEnvio(evento) {
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome')
+        const sobrenome = form.querySelector('.sobrenome')
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+
+        pessoas.push ({
+            nome:nome.nodeValue,
+            sobrenome: sobrenome.nodeValue,
+            peso:peso.nodeValue,
+            altura:altura.nodeValue
+        });
+        console.log(pessoas)
+
+        resultado.innerHTML += `<p>${nome.nodeValue} ${sobrenome.nodeValue} ${peso.nodeValue} ${altura.nodeValue}</p>`
+    };
+    form.addEventListener('submit', recebeEventoEnvio);
+};
+escopo()
