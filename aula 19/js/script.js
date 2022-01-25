d94929a5405cc153f3e89243f6a74bf5af2c4d26
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-01-25 16:29:33 -0300

    Update commit
    Adicionando aula 19

---
+++ aula 19/js/script.js
@@ -0,0 +1,22 @@
+/*
+
+Sobre lógica de programação:
+
+Lógica de programação é a técnica de encadear pensamentos para atingir determinado objetivo. Estes 
+pensamentos, podem ser descritos como uma seqüência de instruções, que devem ser seguidas para se cumprir 
+uma determinada tarefa. Seqüência Lógica são passos executados até atingir um objetivo ou solução de um problema.
+
+Em javascript podemos ter uma base na lógica que podem ser apresentadas em operadores de comparação, estruturas
+de condição e estruturas de repetição.
+
+Operadores de comparação: estes operadores servem para comparar uma um valor com outro. São estes 
+maior que: '>' 
+maior ou igual que: '>='
+menor ou igual que: '<='
+menor que: '<'
+igual a: '=='
+igual a restrito: '==='
+diferente: '!='
+diferente restrito: '!=='
+
+*/
