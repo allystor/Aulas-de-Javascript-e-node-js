24082d5619b87b8021c5940fcf9697b645d14300
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-01-25 20:55:01 -0300

    Update commit
    Adcionando aula 20

--- aula 19/index.js
+++ aula 19/index.js
@@ -3,8 +3,9 @@ Sobre lógica de programação:
 Lógica de programação é a técnica de encadear pensamentos para atingir determinado objetivo. Estes 
 pensamentos, podem ser descritos como uma seqüência de instruções, que devem ser seguidas para se cumprir 
 uma determinada tarefa. Seqüência Lógica são passos executados até atingir um objetivo ou solução de um problema.
-Em javascript podemos ter uma base na lógica que podem ser apresentadas em operadores de comparação, estruturas
-de condição e estruturas de repetição.
+Em javascript podemos ter uma base na lógica que podem ser apresentadas em operadores lógicos, operadores
+de comparação, estruturas de condição e estruturas de repetição.
+
 Operadores de comparação: estes operadores servem para comparar uma um valor com outro. São estes 
 maior que: '>' 
 maior ou igual que: '>='
---
+++ aula 20/index.js
@@ -0,0 +1,21 @@
+/*
+
+Sobre operadores lógicos:
+
+Os operadores lógicos como o próprio nome diz eles realizam eventos de lógica dentro do javascript e servem para
+checar mais de uma operação de um ou mais eventos. Em javascript temos os seguintes operadores lógicos:
+
+&& -> AND -> E (Todas as expressões com '&&' precisam ser verdaderias, caso contrário, se durante uma operação
+                de uma expressão houver falso, toda a expressão será validade como falso)
+
+|| -> OR -> OU
+
+
+! -> NOT -> NÃO
+
+Vejamos como utilizamos estes operadores em valores:
+
+*/
+console.log(true && true);
+
+console.log(true && false)
