130e59d02e595c780374a012cd0ef9a15f194782
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-02-01 15:28:28 -0300

    Update commit
    Adicionando aula 22

---
+++ aula 22/index.js
@@ -0,0 +1,10 @@
+/*
+Sobre estruturas de condição:
+
+Estruturas de condicao é a estrutura de condição a ser verificada para decidir se as linhas do bloco de código 
+serão executadas ou não. Caso a condição seja falsa, o bloco de código 1 não deve ser executado, mas o bloco de 
+código 2 sim. Não existe limite para o número de condições declaradas com a estrutura else if (condicao n) 
+
+Suponhamos que:
+
+*/
